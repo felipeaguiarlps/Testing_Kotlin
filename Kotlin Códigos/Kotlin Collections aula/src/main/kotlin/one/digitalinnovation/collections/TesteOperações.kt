4f53a8cd3a06738ef1523ba609ca25958a65f05d
Felipe Aguiar <96080744+felipeaguiarlps@users.noreply.github.com>
2022-02-16 18:38:30 -0300

    Create collection operators Teste

---
+++ Kotlin Códigos/Kotlin Collections aula/src/main/kotlin/one/digitalinnovation/collections/TesteOperações.kt
@@ -0,0 +1,35 @@
+package one.digitalinnovation.collections
+
+fun main(){
+
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for(salario in salarios){
+        println(salario)
+    }
+        println("-------------------------------------")
+
+    println("Maior salário é o: ${salarios.maxOrNull()}")
+    println("Menor salário é o: ${salarios.minOrNull()}")
+    println("Média Salarial: ${salarios.average()}")
+
+    val salariosMaiorque = salarios.filter { it > 2500.0 }
+    println("-------------------------------------")
+
+    salariosMaiorque.forEach{println (it)}
+
+    println("-------------------------------------")
+
+
+    println(salarios.count{it in 2000.0 .. 5000.0})
+
+    println("-------------------------------------")
+
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 750.0 })
+
+    println("-------------------------------------")
+    println(salarios.any { it == 1000.0 })
+
+
+}
